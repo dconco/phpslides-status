1a8226b7033aaf96dfa9fe33da0708abdba2e8a8
dconco <onipededavid208@gmail.com>
2024-06-30 18:36:00 +0100

    completed Status.php function

--- src/Status.php
+++ src/Status.php
@@ -7,7 +7,11 @@ use PhpSlides\Interface\StatusInterface;
 
 class Status implements StatusInterface
 {
-	protected string $response;
+
+	public int $status = StatusCode::NO_CONTENT;
+	public string $statusText = StatusText::NO_CONTENT;
+	public mixed $message = [];
+	public string $response;
 
 	/**
 	 * @param string $response Set the Response Type for default API response
@@ -21,5 +25,127 @@ class Status implements StatusInterface
 			$this->response = StatusText::UNSUPPORTED_MEDIA_TYPE;
 		}
 		$this->response = $response;
+	}
+
+	// Getters
+
+	public function getStatus (): int
+	{
+		http_response_code($this->status);
+		return $this->status;
+	}
+	public function getStatusText (): string
+	{
+		return $this->response;
+	}
+
+	public function getMessage (): mixed
+	{
+		return $this->message;
+	}
+
+	public function get (): string
+	{
+		$data = [
+		 'status' => $this->status,
+		 'statusText' => $this->statusText,
+		 'message' => $this->message
+		 ];
+
+		switch ($this->response)
+		{
+			case (Response::JSON):
+				$response = Response::json($data, $this->status);
+			case (Response::HTML):
+				$response = Response::html($data, $this->status);
+			case (Response::XML):
+				$response = Response::xml($data, $this->status);
+			case (Response::CSV):
+				$response = Response::csv($data, $this->status);
+			default:
+				$response = Response::text($data, $this->status);
+		}
+
+		return $response;
+	}
+
+	public function getJson (): string
+	{
+		$status = [
+		  'status' => $this->status,
+		  'statusText' => $this->statusText,
+		  'message' => $this->message
+		];
+
+		http_response_code($this->status);
+		header("HTTP/1.1 {$this->status} {$this->statusText}");
+		header("Content-Type: application/json");
+
+		return json_encode($status);
+	}
+
+	// Setters
+	public function set (mixed $data, int $status = StatusCode::NO_CONTENT, string $statusText = StatusText::NO_CONTENT): void
+	{
+		$this->status = $status;
+		$this->statusText = $statusText;
+		$this->message = $data;
+	}
+
+	public function setStatusText (string $statusText): void
+	{
+		$this->statusText = $statusText;
+	}
+
+	public function setStatus (int $status): void
+	{
+		$this->status = $status;
+	}
+
+	public function setMessage (mixed $message): void
+	{
+		$this->message = $message;
+	}
+
+	public function error (array|string $data, int $status = StatusCode::INTERNAL_SERVER_ERROR): string
+	{
+		$data = [ 'error' => $data ];
+
+		switch ($this->response)
+		{
+			case (Response::JSON):
+				$response = Response::json($data, $status);
+			case (Response::HTML):
+				$response = Response::html($data, $status);
+			case (Response::XML):
+				$response = Response::xml($data, $status);
+			case (Response::CSV):
+				$response = Response::csv($data, $status);
+			default:
+				$response = Response::text($data, $status);
+		}
+
+		return $response;
+	}
+
+	public function success (array|string $data, int $status = StatusCode::OK): string
+	{
+		$data = [ 'success' => $data ];
+
+		switch ($this->response)
+		{
+			case (Response::JSON):
+				$response = Response::json($data, $status);
+			case (Response::HTML):
+				$response = Response::html($data, $status);
+			case (Response::XML):
+				$response = Response::xml($data, $status);
+			case (Response::CSV):
+				$response = Response::csv($data, $status);
+			default:
+				$response = Response::text($data, $status);
+		}
+
+		return $response;
 	}
 }
--- src/interface/StatusInterface.php
+++ src/interface/StatusInterface.php
@@ -2,6 +2,8 @@
 
 namespace PhpSlides\Interface;
 
+use PhpSlides\StatusCode;
+use PhpSlides\StatusText;
 use PhpSlides\Enums\Response;
 
 interface StatusInterface
@@ -10,4 +12,27 @@ interface StatusInterface
     * @param string $responseType Set the Response Type for default API response
     */
    public function __construct (string $responseType = Response::JSON);
+
+   public function getStatus (): int;
+
+   public function getStatusText (): string;
+
+
+   public function getMessage (): mixed;
+
+   public function get (): string;
+
+   public function getJson (): string;
+
+   public function set (mixed $data, int $status = StatusCode::NO_CONTENT, string $statusText = StatusText::NO_CONTENT): void;
+
+   public function setStatus (int $status): void;
+
+   public function setStatusText (string $statusText): void;
+
+   public function setMessage (mixed $message): void;
+
+   public function error (array|string $data, int $status = StatusCode::INTERNAL_SERVER_ERROR): string;
+
+   public function success (array|string $data, int $status = StatusCode::OK): string;
 }
